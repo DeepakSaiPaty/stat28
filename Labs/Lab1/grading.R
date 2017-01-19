e95c84d4c64664f9a8511417e5d5ef6ae44b4b47
Boying Gong <jorothy_gong@berkeley.edu>
2017-01-19 08:38:47 -0800

    fix grading bugs in lab 1

---
+++ Labs/Lab1/grading.R
@@ -0,0 +1,7 @@
+library(stringr)
+
+grade = c()
+for (i in 1:7){
+    grade = c(grade, capture.output(test_dir(paste0("q/q", i), reporter="minimal")))
+}
+write.table(sum(str_count(grade, "\\.")), "grade.txt")
