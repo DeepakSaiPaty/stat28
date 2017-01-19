dfd47c79b10c61cc8fd6f3bb313c8bf25874b45b
Boying Gong <jorothy_gong@berkeley.edu>
2017-01-19 10:24:48 -0800

    modify grading

--- Labs/Lab1/grading.R
+++ Labs/Lab1/grading.R
@@ -5,9 +5,3 @@ for (i in 1:7){
     grade = c(grade, capture.output(test_dir(paste0("q/q", i), reporter="minimal")))
 }
 write.table(sum(str_count(grade, "\\.")), "grade.txt")
-
-
-
-as.numeric(factor(c("fri", "two", "leuf")))
-
-dnorm(0, 1, 2)
